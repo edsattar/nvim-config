5c0a7e577f920a79fc62e5775dc2f69762bd0b6e
edsattar <ahmed.sattar@live.com>
2024-05-07 07:08:31 +0600

    .

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,3 +4,16 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 	end,
 })
 
+vim.api.nvim_create_autocmd({"LspAttach"}, {
+  callback = function()
+    local wk = require("which-key")
+    wk.register({
+      g = {
+        name = "Goto",
+        d = { vim.lsp.buf.definition, "Go to definition" },
+        m = { require("telescope.builtin").lsp_references,
+          "Open a telescope window with references" },
+      },
+    }, { buffer = 0 })
+  end
+})
