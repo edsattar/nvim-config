4574a2ba0def0a959e0bf25dfc1a9f0320e808f0
edsattar <ahmed.sattar@live.com>
2024-05-17 03:54:53 +0600

    .

--- lua/plugins/neovim-session-manager.lua
+++ lua/plugins/neovim-session-manager.lua
@@ -1,8 +1,9 @@
 return {
 	"Shatur/neovim-session-manager",
-	-- config = function ()
-	-- 	require('session_manager').setup()
-	--
-	-- 	
-	-- end
+	config = function()
+		local config = require('session_manager.config')
+		require("session_manager").setup({
+			autoload_mode = config.AutoloadMode.Disabled,
+		})
+	end,
 }
