941f33f974014076862ee71fcb8dc2e3094f3250
edsattar <ahmed.sattar@live.com>
2024-09-29 19:05:34 +0600

    add copilot-chat

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = function()
+      return {
+        show_folds = false, -- Shows folds for sections in chat
+        show_help = false, -- Shows help message as virtual lines when waiting for user input
+        question_header = "  User ",
+        answer_header = "  Copilot ",
+        window = {
+          width = 0.3,
+        },
+        selection = function(source)
+          local select = require("CopilotChat.select")
+          return select.visual(source) or select.line(source)
+        end,
+      }
+    end,
+    keys = {
+      {
+        "<leader>c",
+        function()
+          return require("CopilotChat").toggle()
+        end,
+        desc = "Toggle Copilot Chat",
+      },
+    },
+    config = function(_, opts)
+      local chat = require("CopilotChat")
+      require("CopilotChat.integrations.cmp").setup()
+
+      vim.api.nvim_create_autocmd("BufEnter", {
+        pattern = "copilot-chat",
+        callback = function()
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
+        end,
+      })
+
+      chat.setup(opts)
+    end,
+  },
+}
