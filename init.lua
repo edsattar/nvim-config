f003660efe96e334a31b40c3d135b2564e52df53
edsattar <ahmed.sattar@live.com>
2024-10-18 19:34:17 +0600

    .

--- init.lua
+++ init.lua
@@ -1,9 +1,8 @@
-vim.g.mapleader =
-" "                          -- Set <space> as the leader key, Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " " -- Must happen before plugins are loaded
 vim.g.maplocalleader = " "
-vim.g.have_nerd_font = true  -- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.icons_enabled = true
-vim.g.loaded_netrw = 1       -- Disable netrw for neo-tree
+vim.g.loaded_netrw = 1 -- Disable netrw for neo-tree
 vim.g.loaded_netrwPlugin = 1 -- Disable netrwPlugin for neo-tree
 vim.g.markdown_recommended_style = 0
 vim.g.vim_markdown_conceal = 0
@@ -11,9 +10,9 @@ vim.g.vim_markdown_conceal_code_blocks = 0
 vim.g.undotree_DiffCommand = "FC"
 
 vim.filetype.add({
-  pattern = {
-    ["%.env%..*"] = "sh",
-  },
+	pattern = {
+		["%.env%..*"] = "sh",
+	},
 })
 
 _G.user_terminals = {}
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,31 +1,29 @@
 -- attempts to emulate the aesthetics of GUI text editors
 -- https://github.com/akinsho/bufferline.nvim
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		require("bufferline").setup({
-			options = {
-				offsets = {
-					{
-						filetype = "NvimTree",
-						text = "File Explorer",
-						text_align = "left",
-						separator = true,
-						separator_style = "thick",
-					},
-				},
-        name_formatter = function(buf)  -- buf contains:
-          local dir = vim.fn.fnamemodify(buf.path, ":h:t")
-          local bufname = dir .. '/' .. buf.name
-          if #bufname > 17 then
-            return vim.fn.pathshorten(bufname, 4)
-          end
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  opts = {
+    options = {
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          text_align = "left",
+          separator = true,
+          separator_style = "thick",
+        },
+      },
+      name_formatter = function(buf) -- buf contains:
+        local dir = vim.fn.fnamemodify(buf.path, ":h:t")
+        local bufname = dir .. "/" .. buf.name
+        if #bufname > 17 then
+          return vim.fn.pathshorten(bufname, 4)
+        end
 
-          return bufname
-        end,
-			},
-		})
-	end,
+        return bufname
+      end,
+    },
+  },
 }
